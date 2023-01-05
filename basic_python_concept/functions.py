a00b0961a55f4ec23cc3e5e58d34a347efbb3d53
Saurabh Sharma <saurabh.sharma.mis@gmail.com>
2023-01-05 11:52:58 +0000

    Update

--- basic_python_concept/functions.py
+++ basic_python_concept/functions.py
@@ -55,7 +55,11 @@ print ("Outside funtions")
 
 sortStudentName()'''
 
-def sortStudentName():
+##########################################################
+#insidefuntions
+
+
+'''def sortStudentName():
 
 
     print ("Dummy-1")
@@ -65,4 +69,77 @@ def sortStudentName():
 
     print ("Inside funtions")
 
+sortStudentName()'''
+
+##########################################################
+#Vicky Question for input
+
+
+'''def sortStudentName(firstName = "Saurabh", lastName = "Sharma"):
+
+    print ("Dummy1")
+
+print  ("Outside functions")
+
+sortStudentName()'''
+
+
+##########################################################
+#input Function
+
+'''def sortStudentName(firstName = "Saurabh", lastName = "Sharma"):
+
+    print ("Welcome to python " + firstName + " " + lastName )
+
+#print  ("Outside functions")
+
 sortStudentName()
+sortStudentName("Vijay", "Mishra")
+sortStudentName("Saurabh", "Sharma")
+sortStudentName("Vikcy", "Mishra")
+sortStudentName("Preeti", "Mishra")'''
+
+
+##########################################################
+#input Function
+
+'''def greetStudents(firstName = "Saurabh", lastName = "Sharma"):
+
+    print ("Welcome to python " + firstName + " " + lastName )
+
+#print  ("Outside functions")
+
+greetStudents()
+greetStudents("Vijay", "Mishra")
+greetStudents("Saurabh", "Sharma")
+greetStudents("Vikcy", "Mishra")
+greetStudents("Preeti", "Mishra")'''
+
+##########################################################################
+#Question Answer
+
+'''def sortStudentName (firstName = "abc", lastName = "def"):
+    print ("welcome to python " + firstName + " " + lastName)
+    print ("------------------------------------------------")
+    print ("------------------------------------------------")
+
+sortStudentName()
+sortStudentName("abc1 ", "abc2")
+sortStudentName("abc3 ", "abc4")
+sortStudentName("abc5 ", "abc6")
+sortStudentName("abc7 ")'''
+
+#############################################################################
+#SMS
+
+def sortStudentName(firstName = "Ram", lastName = "Sharma"):
+    print ("Welcome to Python " + firstName + " " + lastName )
+    print (" Today is your day in colleage")
+    print (" Please go to noticeboard for timetable")
+    print ("-----------------------------------------------")
+
+sortStudentName()
+sortStudentName("Vijay", "Mishra")
+sortStudentName("Saurabh", "Sharma")
+sortStudentName("Vikcy", "Mishra")
+sortStudentName("Preeti", "Mishra")
