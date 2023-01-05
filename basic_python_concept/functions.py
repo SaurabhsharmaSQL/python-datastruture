6256e546fff6c881d4f1823ccb8544b117a73af6
Saurabh Sharma <saurabh.sharma.mis@gmail.com>
2023-01-05 10:53:28 +0000

    Functionsfilr

---
+++ basic_python_concept/functions.py
@@ -0,0 +1,68 @@
+# camel case
+# all chatacter of the world small exect 1st character of the second and so on words.
+#def SORTNAMES():
+
+#def sortnames():
+
+#def sort_name():
+
+#def sortName():
+
+##########################################################
+
+'''def sortStudentName():
+    #aaa
+    #bbb
+    #ccc
+    #ddd
+    #eee
+
+    print ("Dummy-1")
+    print ("Dummy-2")
+    print ("Dummy-3")
+    print ("Dummy-4")
+
+sortStudentName()'''
+
+##########################################################
+
+'''sortStudentName()
+
+def sortStudentName():
+    #aaa
+    #bbb
+    #ccc
+    #ddd
+    #eee
+
+    print ("Dummy-1")
+    print ("Dummy-2")
+    print ("Dummy-3")
+    print ("Dummy-4")'''
+
+##########################################################
+#Outsidefuntions
+
+'''def sortStudentName():
+
+
+    print ("Dummy-1")
+    print ("Dummy-2")
+    print ("Dummy-3")
+    print ("Dummy-4")
+
+print ("Outside funtions")
+
+sortStudentName()'''
+
+def sortStudentName():
+
+
+    print ("Dummy-1")
+    print ("Dummy-2")
+    print ("Dummy-3")
+    print ("Dummy-4")
+
+    print ("Inside funtions")
+
+sortStudentName()
