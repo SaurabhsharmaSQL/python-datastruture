139a72dd343d146aa063e456623dc745ca30ec80
Saurabh Sharma <saurabh.sharma.mis@gmail.com>
2023-01-06 09:31:03 +0000

    Update

--- basic_python_concept/random_function_202300205.py
+++ basic_python_concept/random_function_202300205.py
@@ -1,4 +1,4 @@
-import random   
+'''import random   
 
 def greetStudentsBySMS(firstName = "Ninja", lastName = "Hathodi"):
     smsSuccessfullySent :bool =False
@@ -28,5 +28,40 @@ print(isSent)
 
 
 isSent = greetStudentsBySMS("Learner.Saurabh",)
-print(isSent)
+print(isSent)'''
 
+############################################################################
+
+import random   
+
+def greetStudentsBySMS(firstName = "Ninja", lastName = "Hathodi"):
+    smsSuccessfullySent :bool =False
+
+    print("Welcome to python -" + firstName + " " + lastName)
+    print("Today is your first day in our Superlearners Class")
+    print("Please prepare your random function program")
+
+    smsSuccessfullySent = bool(random.getrandbits(1))
+
+    print('###########################################')
+
+    return(smsSuccessfullySent, "Message Sent")
+
+#type-1
+
+greetStudentsBySMS()
+
+#type-2
+isSent, megFromFunction = greetStudentsBySMS("Boss.Vijay", "Mishra")
+print(isSent, megFromFunction)
+
+#type-3
+JoBhiReturnKarega = greetStudentsBySMS("Coder.Preeti", "Sharma")
+print(type(JoBhiReturnKarega),JoBhiReturnKarega)
+
+#isSent = greetStudentsBySMS("Decoder.Gaurav", "Sharma")
+#print(isSent)
+
+
+#isSent = greetStudentsBySMS("Learner.Saurabh",)
+#print(isSent)
