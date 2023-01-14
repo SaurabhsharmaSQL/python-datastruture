9f19f413795f5788c42a50782ce32537207cc3cd
Saurabh Sharma <saurabh.sharma.mis@gmail.com>
2023-01-14 11:37:09 +0000

    update

--- basic_python_concept/my_first_code.py
+++ basic_python_concept/my_first_code.py
@@ -1,1 +1,8 @@
 print ("My_First_Python_Code")
+
+numbers = [0,12,25,48,-5,-7,254]
+for num in numbers:
+    if (num < 0):
+     print("Negative num detected")
+    break
+    print(num)
---
+++ basic_python_concept/sum.py
@@ -0,0 +1,9 @@
+int1 = int(input("Please enter 1st Interger: "))
+int2 = int(input("Please enter 2nd integer: "))
+multply = int1*int2
+sum = int1+int2
+if multply <=500:
+    print(multply,end=" ")
+else:
+    print(sum,end=" ")
+print("\n")
