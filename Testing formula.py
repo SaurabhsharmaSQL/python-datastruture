91ebed63c0d61ac4bacf1f3acfee2bc3f443a35c
Saurabh Sharma <saurabh.sharma.mis@gmail.com>
2023-01-21 10:56:23 +0000

    Addon

--- Testing formula.py
+++ Testing formula.py
@@ -1,7 +1,20 @@
-***a=2
-b=2
-a+b
+#***a=2
+#b=2
+#a+b
 
-print(a+b)
-print(a+b*a)***
+#print(a+b)
+#print(a+b*a)***
 
+Sample  = [1,2,3,4,5,[100,200,300,400,500,["poor","fair","good","very good","Excilent"]]]
+
+print(Sample)
+#for i in Sample:
+    #print(i) 
+
+metrix = [[1,2,3],
+          [1,2,3],
+          [1,2,3]]
+
+print(metrix)
+for i in metrix:
+    print(i)
